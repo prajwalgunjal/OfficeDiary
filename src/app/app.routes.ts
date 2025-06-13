8c66e893eaff32eb8a15281015ef775928672a6b
locobuzz-prajwal <prajwal.gunjal@locobuzz.com>
2025-06-13 09:16:43 +0530

    Changed Default routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/dashboard',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
   {
